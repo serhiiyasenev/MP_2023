fa9e59fabe0d6f92b085e146c3d8a67ba6406b18
serhii.yasenev@gmail.com <serhii.yasenev@gmail.com>
2024-01-08 18:21:00 +0200

    public class Form

---
+++ 11_GOFCuttingShape/CuttingShape/Composite/Form.cs
@@ -0,0 +1,22 @@
+﻿namespace _11_GOFCuttingShape.Composite
+{
+    public class Form
+    {
+        string name;
+
+        public Form(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddComponent(string name)
+        {
+            
+        }
+
+        public string ConvertToString()
+        {
+            return "";
+        }
+    }
+}
