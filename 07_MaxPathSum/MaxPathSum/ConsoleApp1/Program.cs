b1a648e6c392b44abc0605882a0c8ce2e1f14c04
Serhii Yasenev <serhii.yasenev@gmail.com>
2024-05-07 22:15:08 +0300

    Recommend (#13)

---
+++ 07_MaxPathSum/MaxPathSum/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[][] nodes = [[5], [7, 3], [6, 8, 10], [12, 9, 13, 16]];
+            var result = Recommend(nodes);
+            Console.WriteLine($"Maximum path sum is: {result}");
+            Console.ReadKey();
+        }
+
+        static int Recommend(int[][] nodes)
+        {
+            if (nodes == null || nodes.Length == 0) return 0;
+
+            int[] maxSum = (int[])nodes[nodes.Length - 1].Clone();
+
+            for (int layer = nodes.Length - 2; layer >= 0; layer--)
+            {
+                for (int i = 0; i <= layer; i++)
+                {
+                    maxSum[i] = nodes[layer][i] + Math.Max(maxSum[i], maxSum[i + 1]);
+                }
+            }
+
+            return maxSum[0];
+        }
+    }
+}
